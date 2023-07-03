cf3c58835b58b1000b4d28d54ad3737afbdb5181
Wildboar14 <92429185+OwenRThomson@users.noreply.github.com>
2023-07-03 15:12:53 +0100

    Create starter.py

---
+++ starter.py
@@ -0,0 +1,15 @@
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.uix.popup import Popup
+
+
+class LogIn(App):
+
+    def build(self):
+
+
+LogIn().run()
